d038c0bf38702abfc94607d5ae1175baf3aad30b
jarek <jarek.mokrzycki@gmai.com>
2018-04-03 15:25:56 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+
+class App extends Component {
+
+  componentDidMount() {
+    fetch('http://api.nbp.pl/api/exchangerates/tables/A')
+    .then(results => {
+      console.log(results.json());
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
