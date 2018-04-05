c9ae38e2f3ebca5837b431a69f818e6b4a3ac93f
jarek <jarek.mokrzycki@gmai.com>
2018-04-05 16:56:52 +0200

    bind update

--- src/components/CurrenciesSelector.js
+++ src/components/CurrenciesSelector.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 export default class CurrenciesTable extends Component {
+  constructor() {
+    super();
+    this.getSelected = this.getSelected.bind(this);
+    this.clearSelected = this.clearSelected.bind(this);
+  }
   getSelected(e){
     this.props.changeSelected(e.target.value, e.target.checked);
   }
@@ -13,7 +18,7 @@ export default class CurrenciesTable extends Component {
     const currenciesLis = currencies.map((currency, index) =>
       <tr key={currency.code + index}>
          <td><input type="checkbox" id={currency.code + '-select'}
-          value={currency.code} onChange={this.getSelected.bind(this)}/></td>
+          value={currency.code} onChange={this.getSelected}/></td>
          <td><label htmlFor={currency.code + '-select'}>{currency.currency}</label></td>
       </tr>
     );
@@ -21,7 +26,7 @@ export default class CurrenciesTable extends Component {
 
     return (
       <div className="currencies-selector-wrapper">
-        <button className="btn btn-dark btn-clear-currencies" onClick={this.clearSelected.bind(this)}>Clear all</button>
+        <button className="btn btn-dark btn-clear-currencies" onClick={this.clearSelected}>Clear all</button>
         <div className="selected-currencies-list">
           <table className="selector-table table table-dark table-hover">
             <tbody>
